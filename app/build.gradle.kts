1520736d4e4203c03ddacf29bc3a71a52a219d89
Mudassir Satti <157783267+MudassirSatti@users.noreply.github.com>
2025-09-27 16:25:06 +0500

    ** first commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,90 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dagger.hilt.plugins)
+    id("kotlin-kapt")
+    id("kotlin-parcelize")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.example.easydiarysatti"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.easydiarysatti"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_11)
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+
+    implementation(libs.androidx.fragment.ktx)
+    implementation(libs.androidx.navigation.fragment.ktx)
+
+
+    // Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.android.compiler.v2481)
+
+    //Glide
+    implementation(libs.glide)
+    //ksp(libs.ksp)
+
+    //dimens
+    implementation(libs.dimens.sdp)
+    implementation(libs.dimens.ssp)
+
+    //room database
+    implementation(libs.room.runtime)
+    ksp(libs.room.compiler)
+
+    //lottie
+    implementation(libs.lottie)
+
+    //shimmer
+    implementation(libs.shimmer)
+    
+}
+
+kapt {
+    correctErrorTypes = true
+    useBuildCache = false
+    generateStubs = true
+}

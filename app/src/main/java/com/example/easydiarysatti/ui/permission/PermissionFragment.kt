149cdf67deed7f844397903ef9201ca5df7f1f02
Mudassir Satti <157783267+MudassirSatti@users.noreply.github.com>
2025-09-28 15:55:00 +0500

    ** implemented language screen

--- app/src/main/java/com/example/easydiarysatti/ui/permission/PermissionFragment.kt
+++ app/src/main/java/com/example/easydiarysatti/ui/permission/PermissionFragment.kt
@@ -3,13 +3,35 @@ package com.example.easydiarysatti.ui.permission
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.easydiarysatti.R
+import com.example.easydiarysatti.databinding.FragmentLanguageBinding
+import com.example.easydiarysatti.databinding.FragmentLanguageBinding.bind
+import com.example.easydiarysatti.databinding.FragmentPermissionBinding
+import com.example.easydiarysatti.safeNav
+import com.example.easydiarysatti.setSelectedBg
+import com.example.easydiarysatti.viewBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PermissionFragment : Fragment(R.layout.fragment_permission) {
+    private val binding by viewBinding(FragmentPermissionBinding::bind)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        clickListener()
+    }
+
+    private fun clickListener() {
+        binding?.apply {
+            btnNext.setOnClickListener { moveToNextScreen() }
+        }
+    }
+
+    fun moveToNextScreen() {
+        findNavController().safeNav(
+            currentDestId = R.id.permissionFragment,
+            actionId = R.id.action_permissionFragment_to_languageFragment
+        )
     }
 }
